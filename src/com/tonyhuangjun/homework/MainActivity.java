0c3e24da1da4a63d2ba3780345ab0fbe6ab4cd0a
boxfactory <tony.huang.jun@gmail.com>
2012-09-08 15:41:13 -0400

    Added more settings for debugging. Added padding to main-four layout. Added handler grabbing and populating of TextViews in MainActivity.

--- src/com/tonyhuangjun/homework/MainActivity.java
+++ src/com/tonyhuangjun/homework/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 	private final static String CLASS_TITLE = "class_title_";
@@ -16,6 +17,13 @@ public class MainActivity extends Activity {
 	private int numberOfClasses;
 	private int layout;
 	private final static String TAG = "MainActivity";
+	
+	private TextView classTitle1, classTitle2, classTitle3,
+		classTitle4, classTitle5, classTitle6, classTitle7,
+		classTitle8;
+	private TextView classBody1, classBody2, classBody3,
+		classBody4, classBody5, classBody6, classBody7,
+		classBody8;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -26,22 +34,58 @@ public class MainActivity extends Activity {
 		SharedPreferences settings = getPreferences(MODE_PRIVATE);
 		Editor editor = settings.edit();
 		editor.putInt(NUMBER_OF_CLASSES, 4);
+		for(int i = 1; i < 9; i++){
+			editor.putString(CLASS_TITLE + i, "Class " + i);
+		}
+		for(int j = 1; j < 9; j++){
+			editor.putString(CLASS_BODY + j, "Description.");
+		}
 		editor.commit();
+		numberOfClasses = settings.getInt(NUMBER_OF_CLASSES, 1);
 		
-		// Select correct corresponding layout for number of 
-		// classes selected
-		switch(settings.getInt(NUMBER_OF_CLASSES, 1)){
-		case 1:
-			layout = R.layout.main_one;
+		// Inflate the appropriate layout
+		switch(numberOfClasses){
+		case 4:
+			layout = R.layout.main_four;	
 			break;
 		case 2:
-			layout = R.layout.main_two;
+			layout = R.layout.main_four;
 			break;
-		case 4:
+		case 1:
 			layout = R.layout.main_four;
 			break;
 		}
+		
 		setContentView(layout);
+		
+		// Initialize the handlers and populate the TextViews.
+		switch(numberOfClasses){
+		case 4:
+			classTitle4 = (TextView) findViewById(R.id.ClassTitle4);
+			classTitle3 = (TextView) findViewById(R.id.ClassTitle3);
+			
+			classTitle4.setText(settings.getString(CLASS_TITLE + 4, "Null"));
+			classTitle3.setText(settings.getString(CLASS_TITLE + 3, "Null"));
+			
+			classBody4 = (TextView) findViewById(R.id.ClassBody4);
+			classBody3 = (TextView) findViewById(R.id.ClassBody3);
+			
+			classBody4.setText(settings.getString(CLASS_BODY + 4, "Null"));
+			classBody3.setText(settings.getString(CLASS_BODY + 3, "Null"));
+		case 2:
+			classTitle2 = (TextView) findViewById(R.id.ClassTitle2);
+			classTitle2.setText(settings.getString(CLASS_TITLE + 2, "Null"));
+			
+			classBody2 = (TextView) findViewById(R.id.ClassBody2);
+			classBody2.setText(settings.getString(CLASS_BODY + 2, "Null"));
+		case 1:
+			classTitle1 = (TextView) findViewById(R.id.ClassTitle1);
+			classTitle1.setText(settings.getString(CLASS_TITLE + 1, "Null"));
+			
+			classBody1 = (TextView) findViewById(R.id.ClassBody1);
+			classBody1.setText(settings.getString(CLASS_BODY + 1, "Null"));
+			break;
+		}
 		
 	}
 
