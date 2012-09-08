8975fde61cb7336f9aa10638f3efd9d2d60dc62d
boxfactory <tony.huang.jun@gmail.com>
2012-09-08 11:19:32 -0400

    Added layouts for two and four classes

--- gen/com/tonyhuangjun/homework/R.java
+++ gen/com/tonyhuangjun/homework/R.java
@@ -12,6 +12,8 @@ public final class R {
     }
     public static final class dimen {
         public static final int alert_text_size=0x7f040001;
+        public static final int class_body_text_size=0x7f040003;
+        public static final int class_title_text_size=0x7f040002;
         public static final int list_side_padding=0x7f040000;
     }
     public static final class drawable {
@@ -19,16 +21,21 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
+        public static final int ClassBody1=0x7f080004;
+        public static final int ClassBody2=0x7f080006;
+        public static final int ClassTitle1=0x7f080003;
+        public static final int ClassTitle2=0x7f080005;
         public static final int empty_list_alert=0x7f080002;
         public static final int homework_list=0x7f080001;
         public static final int list_item=0x7f080000;
-        public static final int menu_edit=0x7f080003;
+        public static final int menu_edit=0x7f080007;
     }
     public static final class layout {
         public static final int list_item=0x7f030000;
         public static final int list_item_bold=0x7f030001;
         public static final int main=0x7f030002;
         public static final int main_one=0x7f030003;
+        public static final int main_two=0x7f030004;
     }
     public static final class menu {
         public static final int main=0x7f070000;
