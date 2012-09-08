d0faf65978c94e8f362abf8356a7b93c9daa9a01
boxfactory <tony.huang.jun@gmail.com>
2012-09-07 23:25:29 -0400

    Added main_one.xml to layout-normal folder in /res/. Removed CustomAdapter.

--- gen/com/tonyhuangjun/homework/R.java
+++ gen/com/tonyhuangjun/homework/R.java
@@ -28,6 +28,7 @@ public final class R {
         public static final int list_item=0x7f030000;
         public static final int list_item_bold=0x7f030001;
         public static final int main=0x7f030002;
+        public static final int main_one=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f070000;
