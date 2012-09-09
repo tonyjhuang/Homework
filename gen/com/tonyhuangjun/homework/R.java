312baa7226534ecf5664a85cec24eb5b6a3b59bb
boxfactory <tony.huang.jun@gmail.com>
2012-09-08 21:42:47 -0400

    Added xml for preferences screen. Added array supplements for preferences.

--- gen/com/tonyhuangjun/homework/R.java
+++ gen/com/tonyhuangjun/homework/R.java
@@ -8,31 +8,36 @@
 package com.tonyhuangjun.homework;
 
 public final class R {
+    public static final class array {
+        public static final int classArray=0x7f050000;
+        public static final int classValues=0x7f050001;
+    }
     public static final class attr {
     }
     public static final class dimen {
-        public static final int alert_text_size=0x7f040001;
-        public static final int class_body_text_size=0x7f040003;
-        public static final int class_title_text_size=0x7f040002;
-        public static final int class_title_vertical_padding=0x7f040004;
-        public static final int layout_side_padding=0x7f040005;
-        public static final int list_side_padding=0x7f040000;
+        public static final int alert_text_size=0x7f060001;
+        public static final int class_body_text_size=0x7f060003;
+        public static final int class_title_text_size=0x7f060002;
+        public static final int class_title_vertical_padding=0x7f060004;
+        public static final int layout_side_padding=0x7f060005;
+        public static final int list_side_padding=0x7f060000;
     }
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int ClassBody1=0x7f080003;
-        public static final int ClassBody2=0x7f080004;
-        public static final int ClassBody3=0x7f080007;
-        public static final int ClassBody4=0x7f080008;
-        public static final int ClassTitle1=0x7f080001;
-        public static final int ClassTitle2=0x7f080002;
-        public static final int ClassTitle3=0x7f080005;
-        public static final int ClassTitle4=0x7f080006;
-        public static final int list_item=0x7f080000;
-        public static final int menu_edit=0x7f080009;
+        public static final int ClassBody1=0x7f0a0004;
+        public static final int ClassBody2=0x7f0a0005;
+        public static final int ClassBody3=0x7f0a0008;
+        public static final int ClassBody4=0x7f0a0009;
+        public static final int ClassTitle1=0x7f0a0002;
+        public static final int ClassTitle2=0x7f0a0003;
+        public static final int ClassTitle3=0x7f0a0006;
+        public static final int ClassTitle4=0x7f0a0007;
+        public static final int TopLayout=0x7f0a0001;
+        public static final int list_item=0x7f0a0000;
+        public static final int menu_edit=0x7f0a000a;
     }
     public static final class layout {
         public static final int list_item=0x7f030000;
@@ -42,15 +47,18 @@ public final class R {
         public static final int main_two=0x7f030004;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f090000;
     }
     public static final class string {
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
-        public static final int menu_edit=0x7f050002;
-        public static final int title_activity_main=0x7f050003;
+        public static final int app_name=0x7f070000;
+        public static final int hello_world=0x7f070001;
+        public static final int menu_edit=0x7f070002;
+        public static final int title_activity_main=0x7f070003;
     }
     public static final class style {
-        public static final int AppTheme=0x7f060000;
+        public static final int AppTheme=0x7f080000;
+    }
+    public static final class xml {
+        public static final int preferences=0x7f040000;
     }
 }
--- src/com/tonyhuangjun/homework/MainActivity.java
+++ src/com/tonyhuangjun/homework/MainActivity.java
@@ -8,78 +8,85 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 	private final static String CLASS_TITLE = "class_title_";
 	private final static String CLASS_BODY = "class_body_";
+	// Class Status: true = unfinished(bold), false = finished(normal)
 	private final static String CLASS_STATUS = "class_status_";
 	private final static String NUMBER_OF_CLASSES = "number_of_classes";
+
 	// As of right now, the possible ints are 1, 2, and 4.
 	private int numberOfClasses;
 	private int layout;
 	private final static String TAG = "MainActivity";
-	
-	private TextView classTitle1, classTitle2, classTitle3,
-		classTitle4, classTitle5, classTitle6, classTitle7,
-		classTitle8;
-	private TextView classBody1, classBody2, classBody3,
-		classBody4, classBody5, classBody6, classBody7,
-		classBody8;
+
+	private SharedPreferences settings;
+	private Editor editor;
+
+	private ViewGroup parent;
+
+	private TextView classTitle1, classTitle2, classTitle3, classTitle4,
+			classTitle5, classTitle6, classTitle7, classTitle8;
+	private TextView classBody1, classBody2, classBody3, classBody4,
+			classBody5, classBody6, classBody7, classBody8;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		// Get preferences file and set number of classes to 4 for
 		// debugging purposes.
-		SharedPreferences settings = getPreferences(MODE_PRIVATE);
-		Editor editor = settings.edit();
-		editor.putInt(NUMBER_OF_CLASSES, 4);
-		for(int i = 1; i < 9; i++){
+		settings = getPreferences(MODE_PRIVATE);
+		editor = settings.edit();
+		editor.putInt(NUMBER_OF_CLASSES, 1);
+		for (int i = 1; i < 9; i++) {
 			editor.putString(CLASS_TITLE + i, "Class " + i);
 		}
-		for(int j = 1; j < 9; j++){
+		for (int j = 1; j < 9; j++) {
 			editor.putString(CLASS_BODY + j, "Description.");
 		}
-		for(int k = 1; k < 9; k++){
+		for (int k = 1; k < 9; k++) {
 			editor.putBoolean(CLASS_STATUS + k, true);
 		}
 		editor.commit();
 		numberOfClasses = settings.getInt(NUMBER_OF_CLASSES, 1);
-		
+
 		// Inflate the appropriate layout
-		switch(numberOfClasses){
+		switch (numberOfClasses) {
 		case 4:
-			layout = R.layout.main_four;	
+			layout = R.layout.main_four;
 			break;
 		case 2:
-			layout = R.layout.main_four;
+			layout = R.layout.main_two;
 			break;
 		case 1:
-			layout = R.layout.main_four;
+			layout = R.layout.main_one;
 			break;
 		}
-		
+
 		setContentView(layout);
-		
+
 		// Initialize the handlers and populate the TextViews.
-		switch(numberOfClasses){
+		switch (numberOfClasses) {
 		case 4:
 			// Get title handlers and set text.
 			classTitle4 = (TextView) findViewById(R.id.ClassTitle4);
 			classTitle3 = (TextView) findViewById(R.id.ClassTitle3);
 			classTitle4.setText(settings.getString(CLASS_TITLE + 4, "Null"));
 			classTitle3.setText(settings.getString(CLASS_TITLE + 3, "Null"));
-			
+
 			// UnFinished? If true, bold. If not, normal.
-			if(settings.getBoolean(CLASS_STATUS + 4, true))
-					classTitle4.setTypeface(Typeface.DEFAULT_BOLD);
-			if(settings.getBoolean(CLASS_STATUS + 3, true))
-					classTitle3.setTypeface(Typeface.DEFAULT_BOLD);
-			
+			if (settings.getBoolean(CLASS_STATUS + 4, true))
+				classTitle4.setTypeface(Typeface.DEFAULT_BOLD);
+			if (settings.getBoolean(CLASS_STATUS + 3, true))
+				classTitle3.setTypeface(Typeface.DEFAULT_BOLD);
+
 			Log.d(TAG, "" + settings.getBoolean(CLASS_STATUS + 4, false));
-			
+
 			// Get body handlers and set text.
 			classBody4 = (TextView) findViewById(R.id.ClassBody4);
 			classBody3 = (TextView) findViewById(R.id.ClassBody3);
@@ -88,24 +95,69 @@ public class MainActivity extends Activity {
 		case 2:
 			classTitle2 = (TextView) findViewById(R.id.ClassTitle2);
 			classTitle2.setText(settings.getString(CLASS_TITLE + 2, "Null"));
-			
-			if(settings.getBoolean(CLASS_STATUS + 2, true))
+
+			if (settings.getBoolean(CLASS_STATUS + 2, true))
 				classTitle2.setTypeface(Typeface.DEFAULT_BOLD);
-			
+
 			classBody2 = (TextView) findViewById(R.id.ClassBody2);
 			classBody2.setText(settings.getString(CLASS_BODY + 2, "Null"));
 		case 1:
 			classTitle1 = (TextView) findViewById(R.id.ClassTitle1);
 			classTitle1.setText(settings.getString(CLASS_TITLE + 1, "Null"));
-			
-			if(settings.getBoolean(CLASS_STATUS + 1, true))
+
+			if (settings.getBoolean(CLASS_STATUS + 1, true))
 				classTitle1.setTypeface(Typeface.DEFAULT_BOLD);
-			
+
 			classBody1 = (TextView) findViewById(R.id.ClassBody1);
 			classBody1.setText(settings.getString(CLASS_BODY + 1, "Null"));
 			break;
 		}
+
+		parent = (ViewGroup) findViewById(R.id.TopLayout);
+
+	}
+
+	public void classTitle4Click(View v) { flip(4); }
+	public void classTitle3Click(View v) { flip(3); }
+	public void classTitle2Click(View v) { flip(2); }
+	public void classTitle1Click(View v) { flip(1); }
+
+	public void flip(int i) {
+		if (settings.getBoolean(CLASS_STATUS + i, true)) {
+			editor.putBoolean(CLASS_STATUS + i, false);
+			switch (i) {
+			case 1:
+				classTitle1.setTypeface(Typeface.DEFAULT);
+				break;
+			case 2:
+				classTitle2.setTypeface(Typeface.DEFAULT);
+				break;
+			case 3:
+				classTitle3.setTypeface(Typeface.DEFAULT);
+				break;
+			case 4:
+				classTitle4.setTypeface(Typeface.DEFAULT);
+				break;
+			}
+		} else {
+			editor.putBoolean(CLASS_STATUS + i, true);
+			switch (i) {
+			case 1:
+				classTitle1.setTypeface(Typeface.DEFAULT_BOLD);
+				break;
+			case 2:
+				classTitle2.setTypeface(Typeface.DEFAULT_BOLD);
+				break;
+			case 3:
+				classTitle3.setTypeface(Typeface.DEFAULT_BOLD);
+				break;
+			case 4:
+				classTitle4.setTypeface(Typeface.DEFAULT_BOLD);
+				break;
+			}
+		}
 		
+		editor.commit();
 	}
 
 	@Override
