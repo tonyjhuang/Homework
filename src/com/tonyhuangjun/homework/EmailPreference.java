187174cb03061cd86a08608416c46011b53d91fa
boxfactory <tony.huang.jun@gmail.com>
2012-10-06 22:44:47 -0400

    Added class skeleton for email preference. Added new preferences category "Development".

---
+++ src/com/tonyhuangjun/homework/EmailPreference.java
@@ -0,0 +1,13 @@
+package com.tonyhuangjun.homework;
+
+import android.content.Context;
+import android.preference.Preference;
+
+public class EmailPreference extends Preference{
+
+    public EmailPreference(Context context) {
+        super(context);
+        // TODO Auto-generated constructor stub
+    }
+    
+}
