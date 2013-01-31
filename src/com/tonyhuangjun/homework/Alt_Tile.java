30be07106caac061a3435e3f90ec3d549ac2d125
Tony Huang <tony.huang.jun@gmail.com>
2013-01-31 18:41:41 -0500

    Added new Tile classes and Layouts (Tile and Alt_Tile)

---
+++ src/com/tonyhuangjun/homework/Alt_Tile.java
@@ -0,0 +1,11 @@
+package com.tonyhuangjun.homework;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+public class Alt_Tile extends LinearLayout{
+    public Alt_Tile(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
---
+++ src/com/tonyhuangjun/homework/Tile.java
@@ -0,0 +1,11 @@
+package com.tonyhuangjun.homework;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+public class Tile extends LinearLayout{
+    public Tile(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
