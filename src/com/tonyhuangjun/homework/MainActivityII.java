b306cfcdcd23e158363407a3aa0b33ae4c74dbd3
Tony Huang <tony.huang.jun@gmail.com>
2013-02-01 18:02:30 -0500

    Tiles are displaying properly, orientation works!

--- src/com/tonyhuangjun/homework/MainActivityII.java
+++ src/com/tonyhuangjun/homework/MainActivityII.java
@@ -2,6 +2,7 @@ package com.tonyhuangjun.homework;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 
 import android.app.AlarmManager;
 import android.app.PendingIntent;
@@ -10,8 +11,12 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.content.res.Resources;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Display;
+import android.view.Surface;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.LinearLayout;
 
 import com.actionbarsherlock.app.SherlockActivity;
@@ -46,7 +51,7 @@ public class MainActivityII extends SherlockActivity {
     AlarmManager am;
 
     LinearLayout layout;
-    
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -78,9 +83,114 @@ public class MainActivityII extends SherlockActivity {
     protected void onResume() {
         super.onResume();
         refreshSettings();
-        
-        layout.addView(new Tile(this, "Class 1", "yo|sup|hah|").getView());
-        
+        drawTiles();
+    }
+
+    private void drawTiles() {
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+
+        // Determine number of Tiles to put in a row or a column.
+        // INVARIANT: If device is rotated, there is at most 2 rows.
+        // INVARIANT: Otherwise, there is at most 2 columns.
+        // rowcolumnSize determines how many rows or columns there should be
+        // with respect to orientation, respecting the invariants listed above.
+        int numOfClass = Integer.valueOf(settings.getString(
+                        NUMBER_OF_CLASSES, "4"));
+        int rowcolumnSize = (int) Math.ceil(numOfClass / 2);
+        // TODO: Come up with some better names. This is so shitty.
+        ArrayList<Tile> leftOrTop = new ArrayList<Tile>();
+        ArrayList<Tile> rightOrBottom = new ArrayList<Tile>();
+
+        // Populate ArrayLists that will supply the Tiles to be drawn later.
+        // Full column/row.
+        for (int i = 1; i - 1 < rowcolumnSize; i++) {
+            leftOrTop.add(new Tile(this, settings.getString(
+                            CLASS_TITLE + i, "Null"), settings
+                            .getString(CLASS_BODY + i, "Null")));
+        }
+        // Rest of the tiles.
+        for (int j = rowcolumnSize + 1; j - 1 < numOfClass; j++) {
+            rightOrBottom.add(new Tile(this, settings.getString(
+                            CLASS_TITLE + j, "Null"), settings
+                            .getString(CLASS_BODY + j, "Null")));
+        }
+
+        // Clear layout to redraw.
+        layout.removeAllViews();
+
+        // Prepare layout params. balancedweight is for child2a, child2b
+        LinearLayout.LayoutParams halfweight = new LinearLayout.LayoutParams(
+                        LayoutParams.MATCH_PARENT,
+                        LayoutParams.MATCH_PARENT, .5f);
+        LinearLayout.LayoutParams balancedweight = new LinearLayout.LayoutParams(
+                        LayoutParams.MATCH_PARENT,
+                        LayoutParams.MATCH_PARENT, (float) 1
+                                        / rowcolumnSize);
+
+
+        // Create ONE LinearLayout if number of classes is EXACTLY 2.
+        // Create THREE LinearLayouts if number of classes is greater than 2.
+        // Push Tile to screen if there is EXACTLY 1.
+        layout = refreshLayoutOrientation(display, layout, false);
+        LinearLayout child2a = null, child2b = null, child1 = null;
+
+        if (numOfClass == 1) {
+            layout.addView(leftOrTop.get(0).getView());
+        } else {
+            child1 = refreshLayoutOrientation(display,
+                            new LinearLayout(this), true);
+            child1.setWeightSum(1f);
+            child1.setLayoutParams(new LinearLayout.LayoutParams(
+                            LayoutParams.MATCH_PARENT,
+                            LayoutParams.MATCH_PARENT, 1f));
+            if (numOfClass == 2) {
+                child1 = addTiles(
+                                addTiles(child1, leftOrTop,
+                                                halfweight),
+                                rightOrBottom, halfweight);
+            } else if (numOfClass > 2) {
+                child2a = refreshLayoutOrientation(display,
+                                new LinearLayout(this), false);
+                child2b = refreshLayoutOrientation(display,
+                                new LinearLayout(this), false);
+                child2a = addTiles(child2a, leftOrTop, balancedweight);
+                child2b = addTiles(child2b, rightOrBottom,
+                                balancedweight);
+                child1.addView(child2a, halfweight);
+                child1.addView(child2b, halfweight);
+            }
+            layout.addView(child1);
+        }
+    }
+
+    private LinearLayout addTiles(LinearLayout layout,
+                    ArrayList<Tile> tiles, LinearLayout.LayoutParams p) {
+        Iterator<Tile> i = tiles.iterator();
+        while (i.hasNext()) {
+            layout.addView(((Tile) i.next()).getView(), p);
+        }
+        return layout;
+    }
+
+    // Updates orientation of layout based on rotation of the screen.
+    // Also takes a boolean. If its true, reverse the orientation of the layout.
+    private LinearLayout refreshLayoutOrientation(Display display,
+                    LinearLayout layout, boolean opposite) {
+        if (display.getRotation() == Surface.ROTATION_90
+                        || display.getRotation() == Surface.ROTATION_270) {
+            if (opposite)
+                layout.setOrientation(LinearLayout.VERTICAL);
+            else
+                layout.setOrientation(LinearLayout.HORIZONTAL);
+        } else {
+            if (opposite)
+                layout.setOrientation(LinearLayout.HORIZONTAL);
+            else
+                layout.setOrientation(LinearLayout.VERTICAL);
+        }
+        return layout;
     }
 
     private void refreshSettings() {
@@ -124,7 +234,7 @@ public class MainActivityII extends SherlockActivity {
 
         for (int i = 1; i < 9; i++) {
             editor.putString(CLASS_TITLE + i, "Class " + i);
-            editor.putString(CLASS_BODY + i, "");
+            editor.putString(CLASS_BODY + i, "enter|your|work|here!|");
             editor.putBoolean(CLASS_STATUS + i, false);
         }
 
--- src/com/tonyhuangjun/homework/Tile.java
+++ src/com/tonyhuangjun/homework/Tile.java
@@ -1,6 +1,7 @@
 package com.tonyhuangjun.homework;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import android.content.Context;
 import android.util.Log;
@@ -11,45 +12,49 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
-public class Tile extends LinearLayout{
+public class Tile extends LinearLayout {
     private String title;
-    private ArrayList<String> body = new ArrayList<String>(); 
+    private ArrayList<String> body = new ArrayList<String>();
     private TextView titleView;
     private ListView bodyView;
     private View view;
-    
-    public Tile(Context context, String _title, String _body) {
+
+    public Tile(Context context, String _title, String _body,
+                    HashMap<String, Integer> colorScheme) {
         super(context);
 
         title = _title;
         body = Interpreter.stringToArrayList(_body);
-        Log.d("TILE", body.toString());
-        
-        view = ((LayoutInflater) context.getSystemService(
-                        Context.LAYOUT_INFLATER_SERVICE))
-                       .inflate(R.layout.tile, null);
-       titleView = (TextView) view.findViewById(R.id.Title);
-       bodyView = (ListView) view.findViewById(R.id.Body);
-       
-       titleView.setText(title);
-       ArrayList<String> aa = new ArrayList<String>();
-       aa.add("hello");
-       aa.add("world");
-       ArrayAdapter<String> arrayAdapter =      
-                       new ArrayAdapter<String>(getContext(),
-                       android.R.layout.simple_list_item_1,body);
-       Log.d("TILE", "before setting");
-       bodyView.setAdapter(arrayAdapter);
-       
-       
+
+        view = ((LayoutInflater) context
+                        .getSystemService(Context.LAYOUT_INFLATER_SERVICE))
+                        .inflate(R.layout.tile, null);
+        titleView = (TextView) view.findViewById(R.id.Title);
+        bodyView = (ListView) view.findViewById(R.id.Body);
+
+        titleView.setText(title);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                        getContext(),
+                        android.R.layout.simple_list_item_1, body);
+        bodyView.setAdapter(arrayAdapter);
+
     }
-    
-    public String getTitle(){ return title; }
-    public String getBody() { return Interpreter.arrayListToString(body); }
-    public View getView()   { return view; }
-    
-    public boolean equals(Tile t){
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getBody() {
+        return Interpreter.arrayListToString(body);
+    }
+
+    public View getView() {
+        return view;
+    }
+
+    public boolean equals(Tile t) {
         return title.equals(t.getTitle())
-                        && Interpreter.arrayListToString(body).equals(t.getBody());
+                        && Interpreter.arrayListToString(body)
+                                        .equals(t.getBody());
     }
 }
